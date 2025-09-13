4da4ab95f5faeb283f5c21196de10f7da11f3fc0
Yash Raj <66545565+ryash072007@users.noreply.github.com>
2025-09-14 01:09:59 +0800

    added cors fix to run lcoally

--- AIAscentBackend/settings.py
+++ AIAscentBackend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "db",
     "api",
     "agents",
+    "corsheaders"
 ]
 
 CACHES = {
@@ -54,11 +55,16 @@ CACHES = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'AIAscentBackend.urls'
